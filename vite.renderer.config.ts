83c337eb6db410562f53f99129358ffdfa96182b
tyler4400 <tyler4400@foxmail.com>
2025-05-27 16:48:08 +0800

    引入tailwind4， 目前@tailwindcss/vite 和 electron forege不兼容。我vite.config里已经魔改了之后可以正常启动。但是会样式还是不生效，准备回退tailwind3

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -3,5 +3,14 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		{
+			name: 'tailwind',
+			async configResolved(config) {
+				const tailwindcss = await import('@tailwindcss/vite');
+				config.plugins.push(tailwindcss.default());
+			}
+		}
+	],
 });
