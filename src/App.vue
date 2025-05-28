376e1b647dbc7f4cd4d0bdf2840025779f94e215
tyler4400 <tyler4400@foxmail.com>
2025-05-28 17:28:03 +0800

    完成新建聊天和应用设置两个button的样式， 与课程中的样式略有不同

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="flex justify-between items-center h-screen">
     <div class="w-[300px] h-full bg-gray-200 text-gray-700 border-r border-gray-300">
-      <ConversationList :items="items" />
+      <div class="h-[calc(100%-60px)] overflow-y-auto">
+        <ConversationList :items="items" />
+      </div>
+      <div class="h-[60px] grid grid-cols-2 gap-2 px-2 items-center">
+        <button
+          class="shadow-sm inline-flex justify-center items-center
+          bg-green-700 text-white text-sm hover:bg-green-700/90
+          border-green-700 rounded-[4px]
+          h-[32px] py-[8px] px-[15px]
+          "
+        >
+          <span>@</span>新建聊天
+        </button>
+        <button
+          class="shadow-sm inline-flex items-center justify-center
+          bg-green-50 text-green-700 hover:bg-green-700 border border-green-700 hover:text-white
+          h-[32px] py-[8px] px-[15px] text-sm rounded-[4px]"
+        >应用设置</button>
+      </div>
     </div>
     <div class="h-full flex-1">
       content
