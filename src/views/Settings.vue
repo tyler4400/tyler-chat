d0d8a0459127ee44850f799317f41703142ae1ba
tyler4400 <tyler4400@foxmail.com>
2025-06-21 21:29:17 +0800

    样式调整

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,9 +3,9 @@
     <h1 class="text-2xl font-bold mb-8">应用设置</h1>
     <div class="space-y-6">
       <div class="setting-item">
-        <label class="block text-sm font-medium text-gray-700 mb-2" >语言设置</label>
-        <SelectRoot v-model="currentConfig.language" class="select-root">
-          <SelectTrigger class="w-44 flex items-center justify-between rounded-md px-3 py-2 text-sm gap-1 bg-white border border-gray-300">
+        <label for="language" class="block text-sm font-medium text-gray-700 mb-2" >语言设置</label>
+        <SelectRoot v-model="currentConfig.language" id="language" class="select-root">
+          <SelectTrigger class="w-40 flex items-center justify-between rounded-md px-3 py-2 text-sm gap-1 bg-white border border-gray-300">
             <SelectValue placeholder="选择语言..." />
             <SelectIcon>
               <Icon icon="radix-icons:chevron-down" />
@@ -36,17 +36,21 @@
         </SelectRoot>
       </div>
       <div class="setting-item">
-        <label class="block text-sm font-medium text-gray-700 mb-2" >字体大小</label>
-        <NumberFieldRoot v-model="currentConfig.fontSize" class="number-root inline-flex">
-          <NumberFieldDecrement class="px-2 border border-r-0 border-gray-300 rounded-l-md hover:bg-green-100 focus:outline-none">
+        <label for="fontSize" class="block text-sm font-medium text-gray-700 mb-2" >字体大小</label>
+        <NumberFieldRoot
+          v-model="currentConfig.fontSize"
+          id="fontSize"
+          class="number-root inline-flex w-40"
+          :min="12"
+          :max="20"
+        >
+          <NumberFieldDecrement class="px-2 border border-r-0 border-gray-300 rounded-l-md hover:bg-green-100 focus:outline-none disabled:bg-gray-50">
             <Icon icon="radix-icons:minus" />
           </NumberFieldDecrement>
           <NumberFieldInput
-              class="number-input w-[60px] px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-green-500 text-center"
-              :min="12"
-              :max="20"
+            class="number-input w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-green-500 text-center"
           />
-          <NumberFieldIncrement class="px-2 border border-l-0 border-gray-300 rounded-r-md hover:bg-green-100 focus:outline-none">
+          <NumberFieldIncrement class="px-2 border border-l-0 border-gray-300 rounded-r-md hover:bg-green-100 focus:outline-none disabled:bg-gray-50">
             <Icon icon="radix-icons:plus" />
           </NumberFieldIncrement>
         </NumberFieldRoot>
